5178f87c91a5845eae5507f0307295a837becb6e
MichalStajerski <m918560@gmail.com>
2020-04-04 22:59:02 +0200

    dodano Lab1

---
+++ Lab1/DB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Lab1
+{
+    class DB
+    {
+        public void Select(SqlConnection connection)
+        {
+            var query = "SELECT * FROM Customers";
+            var command = new SqlCommand(query, connection);
+            using var reader = command.ExecuteReader(); 
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["CompanyName"]);
+
+            }
+        }
+
+        public void Insert(SqlConnection connection, int id, string description)
+        {
+            var query = "INSERT INTO region(regionId, regionDescription) VALUES (@id,@description";
+
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("id", id);
+            command.Parameters.AddWithValue("description", description);
+
+            var affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows affected");
+
+        }
+
+        public void Delete(SqlConnection connection, int id)
+        {
+            var query = "DELETE FROM Customers(FROM Custometrs WHERE regionId =id)";
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("id", id);
+
+            var affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows affected");
+
+
+
+        }
+    }
+
+
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+using System.Data.SqlClient;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new DB();
+            var connectionString = "Data Source=(localdb)/MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using var connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            db.Select(connection);
+            db.Insert(connection, 11, "Bielsko");
+            db.Delete(connection,11);
+
+            connection.Close();
+        }
+    }
+}
